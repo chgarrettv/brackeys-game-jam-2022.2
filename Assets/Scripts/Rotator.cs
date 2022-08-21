ff73bf9486e526145197ac60caa72a1eb457fd92
Mitchell Garrett <45768600+mitchellgarrett@users.noreply.github.com>
2022-08-21 16:38:15 -0600

    Put da planet in da scene

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+    public float degreesPerSecond = 15f;
+    float rotation = 0;
+
+    void Update() {
+        rotation += degreesPerSecond * Time.deltaTime;
+        transform.localRotation = Quaternion.AngleAxis(rotation, Vector3.forward);
+    }
+}
